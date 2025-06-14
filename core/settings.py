e4dafa6765b7bd6deaf0a87b193590e0c9a821d6
Koech01 <01koech@gmail.com>
2025-06-14 12:25:29 +0300

    Updating settings cors.

--- core/settings.py
+++ core/settings.py
@@ -136,8 +136,9 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [ 
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+    'http://127.0.0.1:8000',
     'http://localhost:8000'
 ]
 
