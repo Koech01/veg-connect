0381612fcd81064526c4a2e0ac7b3cfd4f62a18e
Koech01 <01koech@gmail.com>
2025-06-27 23:50:21 +0300

    Update profile url names

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,15 +3,15 @@ from .views import SignUpView, LoginView, ProfileToggleView, ProfileUpdateView, 
 from .views import LogoutView, RefreshApiView, ForgotPasswordView, ResetPasswordView, ProfileUpdatePlantInterestsView
 
 urlpatterns = [
-    path('v1/refresh/'  , RefreshApiView.as_view(), name='refresh'),
-    path('v1/signup/', SignUpView.as_view(), name='signup'),
-    path('v1/login/' , LoginView.as_view(), name='login'),
-    path('v1/logout/', LogoutView.as_view(), name='logout'),
+    path('v1/refresh/'  , RefreshApiView.as_view(), name='refreshView'),
+    path('v1/signup/', SignUpView.as_view(), name='signupView'),
+    path('v1/login/' , LoginView.as_view(), name='loginView'),
+    path('v1/logout/', LogoutView.as_view(), name='logoutView'),
     path('v1/forgot/', ForgotPasswordView.as_view(), name='forgot'),
     path('v1/reset/', ResetPasswordView.as_view(), name='reset'),
-    path('v1/profiles/', ProfileUpdateView.as_view(), name='profileUpdate'), 
+    path('v1/profiles/', ProfileUpdateView.as_view(), name='profileUpdateView'), 
     path('v1/profiles/climate/', ProfileClimateView.as_view(), name='userClimateView'),
-    path('v1/profiles/plants/interests/', OnboardingPlantListView.as_view(), name='userPlantListVoew'),
+    path('v1/profiles/plants/interests/', OnboardingPlantListView.as_view(), name='userPlantListView'),
     path('v1/profiles/plants/choice/', ProfileUpdatePlantInterestsView.as_view(), name='userPlantChoicesView'), 
-    path('v1/profiles/<str:toggleType>/<str:toggleVal>/preference/', ProfileToggleView.as_view(), name='ProfileToggle')
+    path('v1/profiles/<str:toggleType>/<str:toggleVal>/preference/', ProfileToggleView.as_view(), name='ProfileToggleView')
 ]
